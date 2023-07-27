af6da2a128cb7c1209d9cbc1ebdde00367cd5c73
aob <sahan457@cubes.postbox.in.th>
2023-07-27 23:13:23 +0700

    create GetMap API and modify ResponseModel with data

--- src/interface/response.model.ts
+++ src/interface/response.model.ts
@@ -1,9 +1,13 @@
 export class ResponseModel {
     resultCode: string;
     developerMessage: string;
+    data: string;
 
-    constructor(constants) {      
+    constructor(constants,data?) {      
         this.resultCode =  constants.resultCode || constants.response.resultCode;
         this.developerMessage = constants.developerMessage || constants.response.developerMessage;
+        if(data){
+            this.data = data;
+        }
     }
 }
--- src/map/map.controller.ts
+++ src/map/map.controller.ts
@@ -1,4 +1,61 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req, Res, ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { detailHelper, logAppHelper, logStatHelper, summaryHelper } from 'src/common/helpers/logger.helper';
+import { utilityHelper } from 'src/common/helpers/utility.helper';
+import { MapService } from './map.service';
+import { ResponseModel } from 'src/interface/response.model';
+import { ExpectaionFailedException, SuccessException } from 'src/common/exceptions/application.exception';
 
-@Controller('map')
-export class MapController {}
+@Controller('/api/v1/dungeon')
+export class MapController {
+    commandName: string;
+    appName: string = this.config.get('app.name');
+    statApp: any = this.config.get('logger.statPattern')
+    constructor(
+        private readonly config: ConfigService,
+        private readonly logStat: logStatHelper,
+        private readonly mapService: MapService,
+        private readonly validate: ValidationPipe,
+        private readonly utility: utilityHelper
+    ) { }
+    @Get('/map')
+    async GetMap(@Req() req, @Res() res) {
+        this.commandName = 'GetDungeonMap';
+        let identity;
+        const Invoke = this.utility.setValueInitInvoke(req);
+        const Summary = new summaryHelper();
+        const logApp = new logAppHelper();
+        const logDetail = new detailHelper();
+        try {
+            identity = this.utility.CheckIdentity(req, req['TID']);
+            await Summary.logSummaryInit(req, this.commandName, identity);//init Summary
+            await logDetail.logInputRequestDetail(this.appName, this.commandName, req, Invoke, identity);//init Detail
+            await this.logStat.logStatApp(this.statApp.receivedRequest, this.commandName, 'unknown', this.statApp.type.received, req.method, 'success', this.statApp.action.req, '');
+
+            //generate event
+            let dungeonMap = await this.utility.generateDungeon();
+            
+            const message = new ResponseModel(new SuccessException(),Buffer.from(dungeonMap).toString('base64'));
+            await logDetail.logOutputResponseDetail(this.commandName, res, message);
+            await logApp.log('darkdungeonBE --> client :' + JSON.stringify(message), "info");
+            await this.logStat.logStatApp(this.statApp.returnedSuccess, this.commandName, 'unknown', this.statApp.type.returned, req.method, 'success', this.statApp.action.res, '20000');
+            await Summary.logEndSummary(message);
+            res.status(200);
+            res.json(message);
+        } catch (err) {
+            await logApp.log('an error occurred : ' + err, "error");
+            if (err.response) {
+                await Summary.logEndSummary(err.response);
+                await logDetail.logOutputResponseDetail(this.commandName, res, err.response);
+                await this.logStat.logStatApp(this.statApp.returnedError, this.commandName, 'unknown', this.statApp.type.returned, req.method, 'error', this.statApp.action.res, err.response.resultCode);
+                throw err;
+            } else {
+                err = new ResponseModel(new ExpectaionFailedException());
+                await Summary.logEndSummary(err);
+                await logDetail.logOutputResponseDetail(this.commandName, res, err);
+                await this.logStat.logStatApp(this.statApp.returnedError, this.commandName, 'unknown', this.statApp.type.returned, req.method, 'error', this.statApp.action.res, err.resultCode);
+                throw err;
+            }
+        }
+    }
+}
