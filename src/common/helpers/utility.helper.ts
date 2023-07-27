2faa8d80192ac1730208a2521282b8b22bef0aa0
aob <sahan457@cubes.postbox.in.th>
2023-07-27 22:35:49 +0700

    format document utilityhelper

--- src/common/helpers/utility.helper.ts
+++ src/common/helpers/utility.helper.ts
@@ -6,47 +6,34 @@ import { logAppHelper } from '../helpers/logger.helper';
 @Injectable()
 export class utilityHelper {
     constructor(
-        private readonly configService:ConfigService
-    ){}
-    parseJson (string){
-		try {
-			return JSON.parse(string)
-		} catch (error) {
-            console.log('can not parse JSON');        
-			throw new ExpectaionFailedException();
-		}
-	}
+        private readonly configService: ConfigService
+    ) { }
+    parseJson(string) {
+        try {
+            return JSON.parse(string)
+        } catch (error) {
+            console.log('can not parse JSON');
+            throw new ExpectaionFailedException();
+        }
+    }
 
-    setValueInitInvoke (req){
+    setValueInitInvoke(req) {
         let testresponse = req.TID
-		return testresponse
+        return testresponse
     }
 
-    generateRandomChar(){
+    generateRandomChar() {
         var result = [];
-		var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-		var charactersLength = characters.length;
-		for (var i = 0; i < 5; i++) {
-			result.push(characters.charAt(Math.floor(Math.random() * charactersLength)));
-		}
-		return result.join('');
+        var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        var charactersLength = characters.length;
+        for (var i = 0; i < 5; i++) {
+            result.push(characters.charAt(Math.floor(Math.random() * charactersLength)));
+        }
+        return result.join('');
     }
 
-    setInvokeDestNode (){
+    setInvokeDestNode() {
         let node = this.configService.get('app.name');
-		let now = new Date()
-		let year = now.getFullYear()
-		let month = ("0" + (now.getMonth() + 1)).slice(-2);
-		let date = ("0" + now.getDate()).slice(-2)
-		let hour = ("0" + now.getHours()).slice(-2);
-		let minute = (now.getMinutes() < 10 ? '0' : '') + now.getMinutes()
-		let second = (now.getSeconds() < 10 ? '0' : '') + now.getSeconds()
-
-		return `${node}-${year}${month}${date}${hour}${minute}${second}${this.generateRandomChar()}`
-    }
-
-	generateHeader() {
-		let node = this.configService.get('app.name');
         let now = new Date()
         let year = now.getFullYear()
         let month = ("0" + (now.getMonth() + 1)).slice(-2);
@@ -58,14 +45,27 @@ export class utilityHelper {
         return `${node}-${year}${month}${date}${hour}${minute}${second}${this.generateRandomChar()}`
     }
 
-    CheckIdentity(req,identity){
+    generateHeader() {
+        let node = this.configService.get('app.name');
+        let now = new Date()
+        let year = now.getFullYear()
+        let month = ("0" + (now.getMonth() + 1)).slice(-2);
+        let date = ("0" + now.getDate()).slice(-2)
+        let hour = ("0" + now.getHours()).slice(-2);
+        let minute = (now.getMinutes() < 10 ? '0' : '') + now.getMinutes()
+        let second = (now.getSeconds() < 10 ? '0' : '') + now.getSeconds()
+
+        return `${node}-${year}${month}${date}${hour}${minute}${second}${this.generateRandomChar()}`
+    }
+
+    CheckIdentity(req, identity) {
         const logApp = new logAppHelper();
-        logApp.log('CheckIdentity : '+identity, "info");
-        if(!identity){
-            logApp.log( identity+" is Empty!", "debug");
+        logApp.log('CheckIdentity : ' + identity, "info");
+        if (!identity) {
+            logApp.log(identity + " is Empty!", "debug");
             return "undefined";
-        }else{
-            logApp.log( identity+" have value!", "info");
+        } else {
+            logApp.log(identity + " have value!", "info");
             return identity;
         };
     }
@@ -75,22 +75,23 @@ export class utilityHelper {
         let firstNumber = 2;
         let secondNumber = Math.floor(Math.random() * 3) + 1;
         let generatedNumber = firstNumber.toString() + secondNumber.toString();
-      
+
         for (let i = 0; i < length; i++) {
-          firstNumber = validFirstNumbers[Math.floor(Math.random() * validFirstNumbers.length)];
-          secondNumber = Math.floor(Math.random() * 3) + 1;
-          generatedNumber += firstNumber.toString() + secondNumber.toString();
+            firstNumber = validFirstNumbers[Math.floor(Math.random() * validFirstNumbers.length)];
+            secondNumber = Math.floor(Math.random() * 3) + 1;
+            generatedNumber += firstNumber.toString() + secondNumber.toString();
         }
         return generatedNumber;
     }
 
-    generateDungeon(){
+    generateDungeon() {
         //_1 randomNormalEvent _2 mainEvent _3 Door _4 Locked Door _5 specialEvent
         let start = '23'; //Dungeon Entrance
         let end = '21'
-        for(let i = 0 ; i < 10 ; i++ ){
-
+        for (let i = 0; i < 10; i++) {
+            start = start + this.generateSeed(i);
         };
+        return start + end;
     }
 }
 
